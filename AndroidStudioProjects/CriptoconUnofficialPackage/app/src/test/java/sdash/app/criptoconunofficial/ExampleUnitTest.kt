321b574a200123c7fc59e672667bd09095f691e3
glevandowski <33090649+glevandowski@users.noreply.github.com>
2019-03-04 14:33:36 -0300

    Convert to kotlin

---
+++ AndroidStudioProjects/CriptoconUnofficialPackage/app/src/test/java/sdash/app/criptoconunofficial/ExampleUnitTest.kt
@@ -0,0 +1,19 @@
+package sdash.app.criptoconunofficial
+
+import android.util.Log
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        Log.d("Message","Kotlin is life")
+        assertEquals(4, (2 + 2).toLong())
+    }
+}
